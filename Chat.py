0cf17c491e1c592613a63d17db980544457097e1
Arjun Naik <arjunsatishnaik@gmail.com>
2022-06-23 21:57:52 -0700

    Update Chat.py

--- Chat.py
+++ Chat.py
@@ -34,7 +34,6 @@ def run():
     while ip in data.col_values(1):
         time.sleep(BOZO_NUMBER)
         if len(messages.get_all_records()) != 0:
-            print(messages.get_all_records())
             print(f"|{messages.acell('B2').value}||{format_time(messages.acell('D2').value)}| {messages.acell('C2').value}")
             clear_message()
 
